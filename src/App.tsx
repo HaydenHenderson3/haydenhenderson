0b0c328d100fead4020aad577f7f9707754ec71f
Hayden Henderson <hayden.henderson03@gmail.com>
2026-02-08 21:40:47 -0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Reviews from './pages/Reviews'
@@ -8,17 +9,20 @@ import PublishedWorks from './pages/PublishedWorks'
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/reviews" element={<Reviews />} />
-          <Route path="/stories" element={<Stories />} />
-          <Route path="/stories/:id" element={<StoryDetail />} />
-          <Route path="/published-works" element={<PublishedWorks />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/reviews" element={<Reviews />} />
+            <Route path="/stories" element={<Stories />} />
+            <Route path="/stories/:id" element={<StoryDetail />} />
+            <Route path="/published-works" element={<PublishedWorks />} />
+          </Routes>
+        </Layout>
+      </Router>
+      <Analytics />
+    </>
   )
 }
 
